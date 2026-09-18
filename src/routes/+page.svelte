3e84b62c70df6eb9e9373ac3a0df70c41593990f
BlueOceanVale <bluecloudyriver@gmail.com>
2026-09-18 11:53:29 +0530

    Initial commit: setup and make a hero section

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import './layout.css';
+</script>
+
+<section class="flex min-h-screen flex-col items-center justify-center gap-8 bg-black font-sans">
+	<h1 class="font-mono text-5xl font-extrabold tracking-tight text-yellow-500">Luminous Apple</h1>
+	<a
+		href="https://github.com/LuminousApple"
+		class="m-3 rounded-lg bg-amber-400 p-2 px-5 py-2.5 font-bold text-black shadow-lg shadow-amber-400/50 transition-all duration-300 hover:scale-105"
+		>Github Org ↗&#xFE0E;</a
+	>
+</section>
