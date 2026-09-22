5a0eedc3896da26828d4843912871adbef3ea238
BlueOceanVale <bluecloudyriver@gmail.com>
2026-09-22 17:17:42 +0530

    feat: a simple nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
 	import './layout.css';
+	import logo from '$lib/assets/favicon.jpg';
 </script>
 
-<section class="flex min-h-screen flex-col items-center justify-center gap-8 bg-black font-sans">
+<section class="sticky top-0 flex flex-row justify-between bg-black px-10 font-sans">
+	<div class="py-5">
+		<img src={logo} alt="logo" class="h-10 w-10 rounded" />
+	</div>
+	<div class="flex gap-4 py-8">
+		<a href="https://github.com/LuminousApple" class="text-white no-underline">Github</a>
+		<a href="#about" class="text-white no-underline">About</a>
+	</div>
+</section>
+
+<hr class="mt-* w-full border-t border-amber-400 shadow-2xl" />
+
+<section class="flex min-h-screen flex-col items-center gap-8 bg-black pt-80 font-sans">
 	<h1 class="font-mono text-5xl font-extrabold tracking-tight text-yellow-500">Luminous Apple</h1>
 	<a
 		href="https://github.com/LuminousApple"
